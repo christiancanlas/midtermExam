70c5248935a08b8c64c4e6e47466dc2231c49ad4
Christian Canlas <christian.canlas@unc.edu.ph>
2020-02-12 19:50:01 +0800

    Initial Commit

---
+++ dashboard.php
@@ -0,0 +1,76 @@
+<?php
+    include_once 'database.php';
+    session_start();
+    if(!(isset($_SESSION['email'])))
+    {
+        header("location:login.php");
+    }
+    else
+    {
+        $name = $_SESSION['name'];
+        $email = $_SESSION['email'];
+        include_once 'database.php';
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quiz Maker</title>
+    <link  rel="stylesheet" href="css/bootstrap.min.css"/>
+      
+
+    <link  rel="stylesheet" href="css/font.css">
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+
+                <?php
+
+                        echo '<div class="row"><span class="title1" style="margin-left:40%;font-size:30px;color:#fff;"><b>Enter Question</b></span><br /><br />
+                        <div class="col-md-3"></div><div class="col-md-6">   
+                        <form class="form-horizontal title1" name="form" action="update.php?q=addquiz"  method="POST">
+                            <fieldset>
+                                <div class="form-group">
+                                    <label class="col-md-12 control-label" for="name"></label>  
+                                    <div class="col-md-12">
+                                        <input id="name" name="name" placeholder="Enter the question" class="form-control input-md" type="text">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-12 control-label" for="total"></label>  
+                                    <div class="col-md-12">
+                                        <input id="total" name="total" placeholder="Enter Answer" class="form-control input-md" type="text">
+                                    </div>
+                                </div>
+
+                                
+                                <div class="form-group">
+                                    <label class="col-md-12 control-label" for=""></label>
+                                    <div class="col-md-12"> 
+                                        <input  type="submit" style="margin-left:45%" class="btn btn-primary" value="Submit" class="btn btn-primary"/>
+                                    </div>
+                                </div>
+
+                            </fieldset>
+                        </form></div>';
+    
+                ?>
+
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ exam.sql
@@ -0,0 +1,80 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+
+CREATE TABLE `quiz` (
+  `admin_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
+(1, 'admin@gmail.com', 'admin');
+
+
+
+CREATE TABLE `answer` (
+  `qid` text NOT NULL,
+  `ansid` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+CREATE TABLE `questions` (
+  `eid` text NOT NULL,
+  `qid` text NOT NULL,
+  `qns` text NOT NULL,
+  `choice` int(10) NOT NULL,
+  `sn` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+CREATE TABLE `quiz` (
+  `eid` text NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
+CREATE TABLE `user` (
+  `name` varchar(50) NOT NULL,
+  `college` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
